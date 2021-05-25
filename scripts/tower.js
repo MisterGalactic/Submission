7acf7c0a79af95c3665e03ab1d223a5027114a40
Julian.Chan <wyk.julian@gmail.com>
2021-05-25 15:20:12 +0800

    added files

---
+++ scripts/tower.js
@@ -0,0 +1,48 @@
+//Describe tower features
+const towerTypeOne = {
+  $elem: null,
+  id: `_${Math.random().toString(36).substring(2, 15)}`, //scrutiny needed
+  dimension: initDimension,
+  position: initPos,
+  background: initBackground,
+  shooting range: TOWER_RADIUS,
+  damage: TOWER_DP
+  }
+
+const towerTypeTwo = {
+  $elem: null,
+  id: `_${Math.random().toString(36).substring(2, 15)}`, //scrutiny needed
+  dimension: initDimension,
+  position: initPos,
+  background: initBackground,
+  shooting range: TOWER_RADIUS,
+  damage: TOWER_DP
+  }
+
+const towerTypeThree = {
+  $elem: null,
+  id: `_${Math.random().toString(36).substring(2, 15)}`, //scrutiny needed
+  dimension: initDimension,
+  position: initPos,
+  background: initBackground,
+  shooting range: TOWER_RADIUS,
+  damage: TOWER_DP
+  }
+
+
+// map grid alongside pathway
+const towerRoadGrid = [
+  r = [null,null,null,null,null,null,null,null],  // a 8x2 grid
+  c = [null,null,null,null,null,null,null,null]
+]
+
+Let newRow = r, newCol = c
+
+function generateTower(r, c) {
+  if r === c === null {
+    towerRoadGrid[newRow], [newCol]
+  } else
+  return // need to revise here
+}
+
+// append Tower to game screen
